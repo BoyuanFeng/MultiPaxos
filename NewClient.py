1648e3a0286e6493ccae42dee3cd0369b5fb9337
BoyuanFeng <boyuan@umail.ucsb.edu>
2017-12-07 10:40:30 -0800

    added protocol for disconnect and reconnect

--- NewClient.py
+++ NewClient.py
@@ -14,10 +14,11 @@ def Client():
     host,port = '',5555
     print host,port    
     sockLocal = doConnect(host,port)   
-    
+    count = 0
     while True :
         try :
-            msg = str(time.time())
+            count += 1
+            msg = str(count)
             sockLocal.send(msg) 
             print "send msg ok (Client1): ",msg                
             print "recv data from (server1):",sockLocal.recv(1024)
@@ -30,27 +31,11 @@ def Client():
             time.sleep(3) 
         time.sleep(1)
 
-def server():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    sock.bind(('', 6666))  
-    sock.listen(5)  
-    while True:  
-        try:
-            connection,address = sock.accept()  
-            buf = connection.recv(1024)  
-            print "recv data from Client2", buf
-            connection.send(buf)    
-            connection.close()
-        except :
-            traceback.print_exc()
     
 if __name__ == "__main__" :
 
     print "Server1 up"
-    S1 = threading.Thread(target = server, args = ())
     S2 = threading.Thread(target = Client, args = ())
-    S1.start()
     S2.start()
-    S1.join()
     S2.join()
     
--- NewServer.py
+++ NewServer.py
@@ -6,15 +6,20 @@ def server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
     sock.bind(('', 5555))  
     sock.listen(5)  
+    connection,address = sock.accept()  
     while True:  
         try :
-            connection,address = sock.accept()  
-            buf = connection.recv(1024)  
+            buf = connection.recv(1024)
+            if buf == "":
+                connection.close()
+                connection,address = sock.accept()
+				 
             print "recv data from Client1", buf
             connection.send(buf)    
-            connection.close()
         except :
-            traceback.print_exc()
+            connection.close()
+            connection,address = sock.accept()
+            pass
 
 
 def doConnect(host,port):
@@ -25,32 +30,10 @@ def doConnect(host,port):
         pass 
     return sock
         
-def Client():   
-    host,port = '',6666
-    print host,port    
-    sockLocal = doConnect(host,port)   
-    
-    while True :
-        try :
-            msg = str(time.time())
-            sockLocal.send(msg) 
-            print "send msg ok (Client2): ",msg                
-            print "recv data from (server2):",sockLocal.recv(1024)
-        except socket.error :
-            print "\r\nsocket error,do reconnect "
-            time.sleep(3)
-            sockLocal = doConnect(host,port)   
-        except :
-            print '\r\nother error occur '            
-            time.sleep(3) 
-        time.sleep(1)
-
 if __name__ == "__main__" :
     
     print "Server2 up"
     S1 = threading.Thread(target = server, args = ())
-    S2 = threading.Thread(target = Client, args = ())
     S1.start()
-    S2.start()
     S1.join()
-    S2.join()
+ 
